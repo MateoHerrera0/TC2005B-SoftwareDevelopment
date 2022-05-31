a7af5b8f918a42c0ecb1ba572ed23468b37cf3b2
G3RZZZ <73681300+G3RZZZ@users.noreply.github.com>
2022-05-31 12:54:30 -0500

    index.html changes to users view and night image

---
+++ Database/views.sql
@@ -0,0 +1,10 @@
+USE alley_cat_db;
+
+CREATE VIEW users_display
+AS
+SELECT username, activity, lastActive
+	FROM alley_cat_db.users LEFT JOIN alley_cat_db.playerstatistics USING (usernameID);
+    
+#DESCRIBE users_display;
+#SELECT * FROM users_display;
+
