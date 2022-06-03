cfeb582e1885ebb0243ae22bb4f0f42a3b81eafe
G3RZZZ <73681300+G3RZZZ@users.noreply.github.com>
2022-06-02 22:16:36 -0500

    index nav changes

--- Database/views.sql
+++ Database/views.sql
@@ -1,10 +1,10 @@
-USE alley_cat_db;
+USE heroku_29b718f30c1a210;
 
-#CREATE VIEW users_display
+CREATE VIEW users_display
 AS
 SELECT username, activity, lastActive
-	FROM alley_cat_db.users LEFT JOIN alley_cat_db.playerStatistics USING (usernameID);
+	FROM heroku_29b718f30c1a210.users LEFT JOIN heroku_29b718f30c1a210.playerStatistics USING (usernameID);
     
 #DESCRIBE users_display;
-#SELECT * FROM users_display;
+SELECT * FROM users_display;
 
