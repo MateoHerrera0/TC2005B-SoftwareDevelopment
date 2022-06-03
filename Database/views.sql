cb63d6544e473fdc166addb1c561315287e42673
G3RZZZ <73681300+G3RZZZ@users.noreply.github.com>
2022-06-03 11:09:56 -0500

    more alley cat local db views

--- Database/views.sql
+++ Database/views.sql
@@ -1,4 +1,5 @@
 USE heroku_29b718f30c1a210;
+USE alley_cat_db;
 
 CREATE VIEW users_display
 AS
@@ -6,5 +7,10 @@ SELECT username, activity, lastActive
 	FROM heroku_29b718f30c1a210.users LEFT JOIN heroku_29b718f30c1a210.playerStatistics USING (usernameID);
     
 #DESCRIBE users_display;
-SELECT * FROM users_display;
+SELECT * FROM user_display;
 
+SELECT * FROM user_point_stats;
+
+SELECT * FROM user_builder_stats;
+
+SELECT * FROM user_time_played;
