7da4a4d9abf183ab24e76f3be01f698555bd96a5
Mateo Herrera <91641832+MateoHerrera0@users.noreply.github.com>
2022-06-08 20:34:15 -0500

    Removed delta time log

--- VIDEOGAME/SoulLink_V1.0-beta/Assets/Scripts/Menu&LevelSelect/MenuController.cs
+++ VIDEOGAME/SoulLink_V1.0-beta/Assets/Scripts/Menu&LevelSelect/MenuController.cs
@@ -100,6 +100,5 @@ public class MenuController : MonoBehaviour
                 transform.Translate(Vector3.up * speed * Time.deltaTime, Space.World);
             }
         }
-        Debug.Log(Time.deltaTime);
     }
 }
